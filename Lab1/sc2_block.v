fa8a5af1a686a07026ae8c83dccb06566dd8776d
joycechu <joycechu@DESKTOP-1IDM1H6.localdomain>
2020-01-26 14:26:25 -0600

    set up sc2_block module

--- Lab1/sc2_block.v
+++ Lab1/sc2_block.v
@@ -3,5 +3,14 @@
 // Refer to sc_block.v for hints!
 
 module sc2_block(s, cout, a, b, cin);
+    output s, cout;
+    input a, b, cin;
+    wire w1, w2, w3;
+
+    xor xo1(w1, a, b);
+    xor xo2(s, w1, cin);
+    and a1(w2, a, b);
+    and a2(w3, w1, cin);
+    or  o1(cout, w2, w3);
 
 endmodule // sc2_block
