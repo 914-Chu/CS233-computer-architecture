85da8a68f1f39becb1919066b733a3cfee71c455
joycechu <joycechu@DESKTOP-1IDM1H6.localdomain>
2020-01-27 17:26:29 -0600

    update keypad.v and tested

--- Lab1/keypad.v
+++ Lab1/keypad.v
@@ -1,6 +1,13 @@
 module keypad(valid, number, a, b, c, d, e, f, g);
    output 	valid;
    output [3:0] number;
-   input 	a, b, c, d, e, f, g;
+   input 	a, b, c, d, e, f, g;      
+
+   assign valid = ((a^b)^c)&((d^e)^(f^g));
+
+   assign number[3] = valid&((b|c)&f);
+   assign number[2] = valid&(((a|b|c)&e)|(a&f));
+   assign number[1] = valid&((a&f)|(b&d)|(c&(d|e)));
+   assign number[0] = valid&(((a|c)&(d|f))|(b&e));
 
 endmodule // keypad
