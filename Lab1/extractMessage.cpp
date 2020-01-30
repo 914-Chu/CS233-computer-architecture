afb35cde2f5ced0af875fbdcbbb0c3cfd9676d52
joycechu <joycechu@DESKTOP-1IDM1H6.localdomain>
2020-01-29 18:52:27 -0600

    unfinished extractMessage.cpp

--- Lab1/extractMessage.cpp
+++ Lab1/extractMessage.cpp
@@ -15,20 +15,26 @@ char *extractMessage(const char *message_in, int length) {
 
    // allocates an array for the output
    char *message_out = new char[length];
-   int *temp = new int[length];
    for (int i=0; i<length; i++) {
-       message_out[i] = 0;    // Initialize all elements to zero.
-       temp[i] = toBin(message_in[i]);
+   		message_out[i] = 0;    // Initialize all elements to zero.
    }
 
 	// TODO: write your code here
-   char *temp_in = new char[lenght];
-   for (int i = 0; i < length/8; i+=8) {
-       for (int j = 0; j < 8; j++) {
-           	
-       }
-  }
-
-
+   char *temp_in = new char[length];
+   for (int i = 0; i < length; i++) {
+   	for(int j = 8; j > 0; j--) {
+		for(int k = 0; k < length; k++){ 
+			int *temp = new int[8];
+			int sum = 0;
+			int index = 0;
+   			temp[k] = (message[k] >> j) & 1;
+			
+			for(int i = 1; i <= 128; i*=2) {
+				sum += temp[index]*i;
+			      	index++;
+			}
+		}
+	}
+   }
    return message_out;
-}
+}	
