54cc1f0ce152709550d2c3af2bbae0a088405eba
joycechu <joycechu@DESKTOP-1IDM1H6.localdomain>
2020-02-01 21:29:06 -0600

    update logicunit.v and logicunit_tb.v

--- Lab2/logicunit.v
+++ Lab2/logicunit.v
@@ -3,5 +3,13 @@ module logicunit(out, A, B, control);
     output      out;
     input       A, B;
     input [1:0] control;
+    wire w0, w1, w2, w3;
+
+    and a1(w0, A, B);
+    or  o1(w1, A, B);
+    nor no1(w2, A, B);
+    xor x1(w3, A, B);
+    
+    mux4 m1(out, w0, w1, w2, w3, control);
 
 endmodule // logicunit
