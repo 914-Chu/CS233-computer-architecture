4dd5dfb6aa17ff0ee4520c47443d4039de6e4a1c
joycechu <joycechu@DESKTOP-1IDM1H6.localdomain>
2020-02-09 20:51:02 -0600

    finished

--- Lab3/arraySortCheck_control_tb.v
+++ Lab3/arraySortCheck_control_tb.v
@@ -24,32 +24,43 @@ module arraySortCheck_control_test;
 		circuit.rf.r[i] <= i;
 
 
-  // Test a sorted array of length 5
-  # 2 array = 11; length = 5; go = 1;
-  # 10 go = 0;
-  # 20
+  	// Test a sorted array of length 5
+  	# 2 array = 11; length = 5; go = 1;
+  	# 10 go = 0;
+  	# 20
 
 
-  // Test a partially sorted array length 5
-  circuit.rf.r[2] <=  32'd1;
-  circuit.rf.r[3] <=  32'd2;
-  circuit.rf.r[4] <=  32'd3;
-  circuit.rf.r[5] <=  32'd2;
-  circuit.rf.r[6] <=  32'd5;
-  # 2 array = 2; length = 5; go = 1;
-  # 10 go = 0;
-  # 20
+  	// Test a partially sorted array length 5
+  	circuit.rf.r[2] <=  32'd1;
+  	circuit.rf.r[3] <=  32'd2;
+  	circuit.rf.r[4] <=  32'd3;
+  	circuit.rf.r[5] <=  32'd2;
+  	circuit.rf.r[6] <=  32'd5;
+  	# 2 array = 2; length = 5; go = 1;
+  	# 10 go = 0;
+  	# 20
 
-  // Test walking off the edge of array
-	circuit.rf.r[7]  <= 32'd7;
-	circuit.rf.r[8]  <= 32'd8;
-	circuit.rf.r[9]  <= 32'd9;
-	circuit.rf.r[10] <= 32'd6;
-	circuit.rf.r[11] <= 32'd7;
-	# 2 array = 7; length = 3; go = 1;
-	# 10 go = 0;
-  # 20
+  	// Test walking off the edge of array
+  	circuit.rf.r[7]  <= 32'd7;
+  	circuit.rf.r[8]  <= 32'd8;
+  	circuit.rf.r[9]  <= 32'd9;
+  	circuit.rf.r[10] <= 32'd6;
+  	circuit.rf.r[11] <= 32'd7;
+  	# 2 array = 7; length = 3; go = 1;
+  	# 10 go = 0;
+  	# 20
+
         // Add your own testcases here!
+  	circuit.rf.r[7]  <= 32'd7;                                    
+  	circuit.rf.r[8]  <= 32'd8;
+  	circuit.rf.r[9]  <= 32'd9;
+  	circuit.rf.r[10] <= 32'd6;
+  	circuit.rf.r[11] <= 32'd7;
+  	//# 2 reset = 1;
+  	//# 2 reset = 0;
+  	# 2 array = 7; length = 5; go = 1;
+  	# 10 go = 0;
+  	# 20
 
         #10 $finish;
     end
