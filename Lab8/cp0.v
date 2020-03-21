5bb6c1d96846f3d0160f0b16a6a5cae107548777
joycechu <joycechu@DESKTOP-1IDM1H6.localdomain>
2020-03-21 02:13:16 -0500

    not tested

--- Lab8/cp0.v
+++ Lab8/cp0.v
@@ -14,4 +14,41 @@ module cp0(rd_data, EPC, TakenInterrupt,
     input         MTC0, ERET, TimerInterrupt, clock, reset;
 
     // your Verilog for coprocessor 0 goes here
+    wire [31:0] status_register;
+    assign status_register[31:16] = 1'b0;
+    assign status_register[15:8] = user_status[15:8];
+    assign status_register[7:2] = 1'b0;
+    assign status_register[1] = exception_level;
+    assign status_register[0] = user_status[0];
+    
+    wire [31:0] cause_register;  
+    assign cause_register[31:16] = 1'b0;
+    assign cause_register[15] = TimerInterrupt;
+    assign cause_register[14:0] = 1'b0;
+
+    wire [31:0] mtc0_dec_out;
+    wire bit12 = mtc0_dec_out[12];
+    wire bit14 = mtc0_dec_out[14];
+    wire [29:0] epc_D;
+    wire [31:0] user_status;
+    wire exception_level;
+    wire el_reset = reset | ERET;
+    wire epc_enable = TakenInterrupt | bit14;
+    
+    wire a0 = cause_register[15] & status_register[15];
+    wire a1 = (~status_register[1]) & status_register[0];
+    assign TakenInterrupt = a0 & a1;
+ 
+    decoder32 mtc0_decoder(mtc0_dec_out, regnum, MTC0);
+   
+    mux2v #(30) wr_nextpc_mux(epc_D, wr_data[31:2], next_pc, TakenInterrupt);
+    
+    register #(32) us(user_status, wr_data, clock, bit12, reset);
+
+    dffe el(exception_level, 1'b1, clock, TakenInterrupt, el_reset);
+
+    register #(30) epc(EPC, epc_D, clock, epc_enable, reset);
+
+    mux32v #(32) rd(rd_data, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, status_register, cause_register, {EPC, 2'b0}, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, regnum); 
+
 endmodule
--- Lab8/machine.v
+++ Lab8/machine.v
@@ -17,18 +17,44 @@ module machine(clk, reset);
    wire         PCSrc, zero, negative;
    wire [31:0]  rd1_data, rd2_data, B_data, alu_out_data, load_data, wr_data;
 
+   wire TimerInterrupt, TimerAddress;
+   wire [31:0] tdata = rd2_data;
+   wire [31:0] taddress = alu_out_data;
+   wire NotIO = ~TimerAddress;
 
-   register #(30, 30'h100000) PC_reg(PC[31:2], next_PC[31:2], clk, /* enable */1'b1, reset);
+   timer timer_(TimerInterrupt, load_data, TimerAddress, tdata, taddress, 
+                MemRead, MemWrite, clk, reset);
+
+   wire [31:0] c0rd_data;
+   wire [29:0] EPC;
+   wire [31:0] c0wr_data = rd2_data;
+   wire [4:0]  c0regnum = rd;
+   wire TakenInterrupt;
+
+   cp0 cp_0(c0rd_data, EPC, TakenInterrupt, c0wr_data, c0regnum, next_PC, 
+            MTC0, ERET, TimerInterrupt, clk, reset);
+   
+   wire [31:0] adr = 32'h80000180;
+   wire [29:0] intrp_adr = adr[31:2]; 
+   wire [29:0] eret_mux_out;
+   wire [29:0] intrp_mux_out; 
+
+   register #(30, 30'h100000) PC_reg(PC[31:2], intrp_mux_out, clk, /* enable */1'b1, reset);
    assign PC[1:0] = 2'b0;  // bottom bits hard coded to 00
    adder30 next_PC_adder(PC_plus4, PC[31:2], 30'h1);
    adder30 target_PC_adder(PC_target, PC_plus4, imm[29:0]);
    mux2v #(30) branch_mux(next_PC, PC_plus4, PC_target, PCSrc);
    assign PCSrc = BEQ & zero;
 
+   mux2v #(30) eret_mux(eret_mux_out, next_PC, EPC, ERET);
+   mux2v #(30) intrp_mux(intrp_mux_out, eret_mux_out, 
+                         intrp_adr, TakenInterrupt);
+
    instruction_memory imem (inst, PC[31:2]);
 
-   mips_decode decode(ALUOp, RegWrite, BEQ, ALUSrc, MemRead, MemWrite, MemToReg, RegDst, MFC0, MTC0, ERET,
-                      inst);
+   mips_decode decode(ALUOp, RegWrite, BEQ, ALUSrc, MemRead, 
+                     MemWrite, MemToReg, RegDst, MFC0, MTC0, 
+                     ERET,inst);
 
    regfile rf (rd1_data, rd2_data,
                rs, rt, wr_regnum, wr_data,
@@ -37,9 +63,12 @@ module machine(clk, reset);
    mux2v #(32) imm_mux(B_data, rd2_data, imm, ALUSrc);
    alu32 alu(alu_out_data, zero, negative, ALUOp, rd1_data, B_data);
 
-   data_mem data_memory(load_data, alu_out_data, rd2_data, MemRead, MemWrite, clk, reset);
-
-   mux2v #(32) wb_mux(wr_data, alu_out_data, load_data, MemToReg);
+   data_mem data_memory(load_data, alu_out_data, rd2_data, 
+                        (MemRead & NotIO), (MemWrite & NotIO), clk, reset);
+   
+   wire [31:0] mtr_mux_out;
+   mux2v #(32) mtr_mux(mtr_mux_out, alu_out_data, load_data, MemToReg);
+   mux2v #(32) mfc0_mux(wr_data, mtr_mux_out, c0rd_data, MFC0);
    mux2v #(5) rd_mux(wr_regnum, rt, rd, RegDst);
 
 endmodule // machine
