9b7032750b21ca42fa281ed259d17e7cce7fc2e3
joycechu <joycechu@DESKTOP-1IDM1H6.localdomain>
2020-04-13 08:46:48 +0800

    tested

--- Lab11/cacheblock.cpp
+++ Lab11/cacheblock.cpp
@@ -2,5 +2,7 @@
 
 uint32_t Cache::Block::get_address() const {
   // TODO
-  return 0;
+  uint32_t tag_shift = _cache_config.get_num_block_offset_bits() + _cache_config.get_num_index_bits();
+  uint32_t index_shift = _cache_config.get_num_block_offset_bits();
+  return (_tag << tag_shift) | (_index << index_shift);
 }
