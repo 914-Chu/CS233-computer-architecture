a165715b81e734e8b924f39b52e9fbacc87b5f43
joycechu <joycechu@DESKTOP-1IDM1H6.localdomain>
2020-04-16 10:57:29 +0800

    not tested

--- Lab12/transpose.cpp
+++ Lab12/transpose.cpp
@@ -3,17 +3,40 @@
 #include <stdlib.h>
 #include <time.h>
 #include "transpose.h"
-
+#include <iostream>
 // will be useful
 // remember that you shouldn't go over SIZE
 using std::min;
 
 // modify this function to add tiling
 void
-transpose_tiled(int **src, int **dest) {
-    for (int i = 0; i < SIZE; i ++) {
-        for (int j = 0; j < SIZE; j ++) {
-            dest[i][j] = src[j][i];
+transpose_tiled(int **src, int **dest) 
+{
+    int bs = 5;
+ 
+    for(int j = 0; j < SIZE; j += bs){
+        for(int i = 0; i < SIZE; i += bs){
+            for (int y = j; y < min(j+bs, SIZE); y ++) {
+                for (int x = i; x < min(i+bs, SIZE); x ++) {
+                    dest[x][y] = src[y][x];
+                    //std::cout << x << " " << y << " " << std::endl;
+                    //dest[x][y] = src[y][x];
+                }
+            }
         }
     }
+
+/*
+    for(int i = 0; i < SIZE; i += bs){                                       
+        for(int j = 0; j < SIZE; j += bs){                                      
+            for (int x = i; x < min(i+bs, SIZE); x ++) {                        
+                for (int y = j; y < min(j+bs, SIZE); y ++) {                   
+                   dest[x][y] = src[y][x];                                      
+                   //std::cout << x << " " << y << " " << std::endl;            
+                   //dest[x][y] = src[y][x];                                   
+                }                                                              
+            }                                                                  
+       }                                                                        
+   }  
+*/
 }
